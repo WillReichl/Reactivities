174c64433ee00bd559904da167c8bf5aeca490ba
Will Reichl <William.Reichl@dcsg.com>
2020-06-28 15:06:29 -0400

    Section 5: Activity States, Button Events/Handlers

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,16 +2,40 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
 
 interface IProps {
   activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  setSelectedActivity,
+  editMode,
+  setEditMode
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-          <ActivityList activities={activities} />
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && <ActivityForm setEditMode={setEditMode} />}
       </Grid.Column>
     </Grid>
   );
